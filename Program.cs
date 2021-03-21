b2d089eda66d5d5c9a7a36ac52adfeb477c427ac
Destroyer <kasatkinilyaandreevich@gmail.com>
2021-03-21 17:12:18 +0300

    Added new code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,101 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
+        static int remotePort;
+        static IPAddress ipAddress;
+        static Socket listeningSocket;
+        static bool flager = true;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ipAddress = IPAddress.Parse("127.0.0.1");
+            remotePort = Int32.Parse("8080");
+            Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+            Console.WriteLine();
+
+            try
+            {
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(Listen);
+                listeningTask.Start();
+
+                while (flager)
+                {
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    EndPoint remotePoint = new IPEndPoint(ipAddress, remotePort);
+                    switch (message)
+                    {
+                        case "1":
+                            listeningSocket.SendTo(data, remotePoint);
+                            break;
+                        case "2":
+                            listeningSocket.SendTo(data, remotePoint);
+                            break;
+                        case "exit":
+                            flager = false;
+                            Close();
+                            break;
+                        default:
+                            Console.WriteLine("Incorrect select");
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void Listen()
+        {
+            try
+            {
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 0);
+                listeningSocket.Bind(localIP);
+
+                while (flager)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listeningSocket.Available > 0);
+
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(), remoteFullIp.Port, builder.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void Close()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+
+            Console.WriteLine("Сервер остановлен!");
         }
     }
 }
